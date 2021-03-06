6664ea356305a46d4a5a67a1e8b44434f6b3b849
VadimKurenev <79751891+VadimKurenev@users.noreply.github.com>
2021-03-06 15:36:58 +0500

    Add files via upload

---
+++ 1.8.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    int t;
+    do
+    {
+        cout << "Введите время t = ";
+        cin >> t;
+
+        if (cin.fail() || t <= 0)
+        {
+            cin.clear();
+            cin.ignore(10, '\n');
+            cout << "Ошибка! Неверный ввод данных!" << endl;
+        }
+        else
+        {
+            break;
+        }
+
+    } while (true);
+
+    int v;
+    do
+    {
+        cout << "Введите скорость V = ";
+        cin >> v;
+
+        if (cin.fail() || v <= 0)
+        {
+            cin.clear();
+            cin.ignore(10, '\n');
+            cout << "Ошибка! Неверный ввод данных!" << endl;
+        }
+        else
+        {
+            break;
+        }
+
+    } while (true);
+
+    double a = v / t;
+    double s, s_1, s_2, s_save;
+    s_1 = v * t;
+    s_2 = (a * pow(t, 2)) / 2;
+    s = s_1 + s_2;
+    cout << "Путь пройденный поездом = " << s << endl;
+    s_save = (3 * v * t) / 2;
+    cout << "Путь пройденный поездом проверка = " << s_save << endl;
+}
---
+++ 2.8.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+
+	int n;
+	do
+	{
+		cout << "Введите число n = ";
+		cin >> n;
+
+		if (cin.fail() || n <= 0)
+		{
+			cin.clear();
+			cin.ignore(36, '\n');
+			cout << "Некорректный ввод, повторите ввод" << endl;
+		}
+		else
+		{
+			break;
+		}
+
+	} while (true);
+
+
+	long int factorial = 1;
+	long int composition = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		composition = n - pow(2, i);
+		factorial *= composition;
+	}
+	cout << "Двойной факториал числа " << n << "!! = " << factorial << endl;
+
+}
