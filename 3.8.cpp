cd27dbb174ffac522b8707a0ca412d2568bff3ba
VadimKurenev <79751891+VadimKurenev@users.noreply.github.com>
2021-03-21 21:45:34 +0500

    Add files via upload

---
+++ 3.8.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int value();
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+
+	int n = value();
+	double m_1 = 0, m_2 = 0, d = 0;
+
+	double* e = new double[n];
+	for (int i = 0; i < n; i++)
+	{
+		do
+		{
+			cout << "Введите вероятность " << i + 1 << ": ";
+			cin >> e[i];
+			if (cin.fail() || e[i] < 0)
+			{
+				cin.clear();
+				cin.ignore(10, '\n');
+				cout << "Значение введено не корректно " << endl;
+			}
+			else
+			{
+				break;
+			}
+		} while (true);
+	}
+	cout << endl;
+
+	double* p = new double[n];
+	double sum_1 = 0;
+	for (int i = 0; i < n; i++)
+	{
+		do
+		{
+			cout << "Введите вероятность " << i + 1 << ": ";
+			cin >> p[i];
+			if (cin.fail() || p[i] < 0 || p[i] > 1)
+			{
+				cin.clear();
+				cin.ignore(10, '\n');
+				cout << "Значение введено не корректно " << endl;
+			}
+			else
+			{
+				break;
+			}
+		} while (true);
+		sum_1 += p[i];
+		sum_1 > 1 ? p[i] = 0 : sum_1 = sum_1;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		m_1 += e[i] * p[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		m_2 += pow(e[i], 2) * p[i];
+	}
+
+	d = m_2 - pow(m_1, 2);
+
+	delete[] e;
+	delete[] p;
+	cout << "Дисперсия случайно величины = " << d << endl;
+}
+int value()
+{
+	int n;
+	while (true)
+	{
+		cout << "Введите количество элементов массива: ";
+		cin >> n;
+		if (cin.fail() || n < 0)
+		{
+			cin.clear();
+			cin.ignore(10, '\n');
+			cout << "Значение введено не корректно " << endl;
+		}
+		else
+		{
+			return n;
+		}
+	}
+}
---
+++ 4.8.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+int value();
+double factorial(int n);
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	int n = value();
+	cout << "Двойной факториал числа " << n << "! = " << factorial(n) << endl;
+}
+double factorial(int n)
+{
+	double factorial = n;
+	double composition;
+	if (n == 1)
+	{
+		factorial = n;
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			composition = n - pow(2, i);
+			factorial *= composition;
+		}
+	}
+	return factorial;
+}
+int value()
+{
+	int n;
+	while (true)
+	{
+		cout << "Введите число n = ";
+		cin >> n;
+
+		if (cin.fail() || n <= 0)
+		{
+			cin.clear();
+			cin.ignore(36, '\n');
+			cout << "Некорректный ввод, повторите ввод" << endl;
+		}
+		else
+		{
+			return n;
+		}
+	}
+}
