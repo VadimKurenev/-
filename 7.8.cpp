a30ab60a814019d758c37a1db237188a9f68dcbd
VadimKurenev <79751891+VadimKurenev@users.noreply.github.com>
2021-04-18 17:19:09 +0500

    Add files via upload

---
+++ 7.8.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+class dbl_fact
+{
+public:
+    void calc(long int n)
+    {
+        long double res = n;
+
+        for (double i = (n - 2); i > 0; i -= 2) res *= i;
+
+        cout << "\nДвойной факториал числа " << n << "!! = " << res << endl;
+    }
+};
+
+long int value()
+{
+    {
+        int n;
+        while (true)
+        {
+            cout << "Введите целочисленное число n = ";
+            cin >> n;
+
+            if (cin.fail() || n <= 0)
+            {
+                cin.clear();
+                cin.ignore(4000, '\n');
+                cout << "Некорректный ввод, повторите ввод" << endl;
+            }
+            else
+            {
+                return n;
+            }
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    cout << "Данная программа вычисляет двойной факториал числа n" << endl;
+
+    long int n = value();
+
+    dbl_fact add;
+    add.calc(n);
+
+    return 0;
+}
---
+++ 8.8.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <cmath>
+#define Pi 3.14
+
+using namespace std;
+
+class complex_class
+{
+public:
+    double Re;
+    double Im;
+
+    void algebr_form()
+    {
+        cout << "\nКомплексное число z в алгебраической форме: " << Re << " + i" << Im << endl;
+    }
+
+    void trig_to_algebr_form()
+    {
+        double fi = atan(Im / Re) * 180.0 / Pi;
+        double p = sqrt(Re * Re + Im * Im);
+
+        cout << "Тригонометрическая форма числа z: " << p << "(cos(" << fi << ") + i * sin(" << fi << "))" << endl;
+    }
+};
+
+double value()
+{
+    while (true)
+    {
+        double value;
+        cin >> value;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(4000, '\n');
+            cout << "Некорректный ввод, повторите ввод: ";
+        }
+        else
+        {
+            cin.ignore(4000, '\n');
+            return value;
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    complex_class a;
+
+    cout << "Введите число x: ";
+    a.Re = value();
+
+    cout << "Введите число y: ";
+    a.Im = value();
+
+    a.algebr_form();
+    a.trig_to_algebr_form();
+
+    return 0;
+}
